66085c8cbd547cbeb96a90f03bd9f0ded3aaf3d5
Egor <koropeczegor@gmail.com>
2024-03-01 22:42:42 +0300

    correction fix, add loading

--- src/js/app.js
+++ src/js/app.js
@@ -1,78 +1,97 @@
-const BASE_URL = "http://api.valantis.store:40000/";
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-let offset = 0;
-const offsetCount = 50;
+"use strict";
+window.addEventListener("DOMContentLoaded", async () => {
+  const BASE_URL = "http://api.valantis.store:40000/";
+  const left = document.querySelector(".left");
+  const right = document.querySelector(".right");
+  const loading = document.querySelector(".loading");
+  let isLoading;
+  let offset = 0;
+  const offsetCount = 50;
 
-left.addEventListener("click", () => {
-  if (offset === 0) {
-    offset;
-  } else {
+  trigerLoading(true);
+  const products = await getProduct();
+
+  trigerLoading(false);
+  function trigerLoading(triger) {
+    if (triger) {
+      isLoading = true;
+      loading.style.display = "flex";
+      return;
+    }
+    isLoading = false;
+    loading.style.display = "none";
+  }
+  left.addEventListener("click", async () => {
+    if (offset === 0) return;
     offset -= offsetCount;
+    trigerLoading(true);
+    await getProduct();
+    trigerLoading(false);
+  });
+
+  right.addEventListener("click", async () => {
+    if (products?.result.length < offsetCount) return;
+    offset += offsetCount;
+    trigerLoading(true);
+    await getProduct();
+    trigerLoading(false);
+  });
+
+  async function apiRequest(url, body) {
+    const _apiKey = "Valantis";
+    const _date = new Date().toISOString().replace(/-/g, "").split("T")[0];
+    const password = md5(`${_apiKey}_${_date}`);
+
+    const resp = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth": password,
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await resp.json();
+
+    return data;
   }
 
-  getProduct();
+  async function getProduct() {
+    const bodyIds = {
+      action: "get_ids",
+      params: { offset, limit: offsetCount },
+    };
+    const respData = await apiRequest(BASE_URL, bodyIds);
+
+    const bodyItems = {
+      action: "get_items",
+      params: { ids: respData.result },
+    };
+    const products = await apiRequest(BASE_URL, bodyItems);
+    showProduct(products);
+    return products;
+  }
+
+  function showProduct(data) {
+    const productsEl = document.querySelector(".products");
+
+    data?.result.forEach((product) => {
+      const productEl = document.createElement("div");
+      productEl.classList.add("product");
+      productEl.innerHTML = `
+      <div id="${product.id}" class="product-card">
+      <h3>${(() => {
+        if (product.brand === null) {
+          return "Goold";
+        } else {
+          return product.brand;
+        }
+      })()}</h3>
+      <p>${product.price}</p>
+      <p>${product.product}</p>
+      </div>
+      
+      `;
+      productsEl.appendChild(productEl);
+    });
+  }
 });
-right.addEventListener("click", () => {
-  offset += offsetCount;
-  getProduct();
-});
-
-getProduct();
-
-async function apiRequest(url, body) {
-  const _apiKey = "Valantis";
-  const _date = new Date().toISOString().replace(/-/g, "").split("T")[0];
-  const password = md5(`${_apiKey}_${_date}`);
-
-  const resp = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Auth": password,
-    },
-    body: JSON.stringify(body),
-  });
-  const data = await resp.json();
-
-  return data;
-}
-async function getProduct() {
-  const bodyIds = {
-    action: "get_ids",
-    params: { offset, limit: 1000 },
-  };
-  const respData = await apiRequest(BASE_URL, bodyIds);
-
-  const bodyItems = {
-    action: "get_items",
-    params: { ids: respData.result },
-  };
-  const items = await apiRequest(BASE_URL, bodyItems);
-  showProduct(items);
-}
-
-function showProduct(data, rowPerPage, page) {
-  const productsEl = document.querySelector(".products");
-  document.querySelector(".products").innerHTML = "";
-
-  data.result.forEach((product) => {
-    const productEl = document.createElement("div");
-    productEl.classList.add("product");
-    productEl.innerHTML = `
-    <div id="${product.id}" class="product-card">
-    <h3>${(() => {
-      if (product.brand === null) {
-        return "Goold";
-      } else {
-        return product.brand;
-      }
-    })()}</h3>
-    <p>${product.price}</p>
-    <p>${product.product}</p>
-    </div>
-    
-    `;
-    productsEl.appendChild(productEl);
-  });
-}
